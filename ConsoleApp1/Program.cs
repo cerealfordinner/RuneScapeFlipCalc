b6597a7e8503766d0a24e452c0c85b907f66bcdf
cerealfordinner <cerealfordinnerx@gmail.com>
2022-03-29 17:28:59 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Figgle;
+
+string item;
+int buyPrice;
+int sellPrice;
+int geLimit;
+
+string askString(string text)
+{
+    Console.WriteLine(text);
+    return (Console.ReadLine());
+}
+
+Console.WriteLine(
+    FiggleFonts.Standard.Render("RuneScape Flip Calc"));
+
+item = askString("Please provide the item you wish to flip");
+
+
+
