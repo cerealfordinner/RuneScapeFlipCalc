ed642fc7b920966054e252601de2551f90e7fae3
cerealfordinner <cerealfordinnerx@gmail.com>
2022-03-29 17:42:37 -0400

    Added user input and logic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,10 +11,16 @@ string askString(string text)
     return (Console.ReadLine());
 }
 
+
 Console.WriteLine(
     FiggleFonts.Standard.Render("RuneScape Flip Calc"));
 
-item = askString("Please provide the item you wish to flip");
+item = askString("Please provide the item you wish to flip:");
+buyPrice = Convert.ToInt32(askString("Buy price:"));
+sellPrice = Convert.ToInt32(askString("Sell price:"));
+geLimit = Convert.ToInt32(askString("GE Limit:"));
+
+Console.WriteLine($"If you flip {geLimit} {item} for the provided prices you will make {(sellPrice - buyPrice) * geLimit}");
 
 
 
