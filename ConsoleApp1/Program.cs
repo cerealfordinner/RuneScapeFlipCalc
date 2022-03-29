8ffb8bb3b8119465a00f1e57de052a9fee7124f5
cerealfordinner <cerealfordinnerx@gmail.com>
2022-03-29 18:05:22 -0400

    Added logic to ensure an int is entered for buyPrice, sellPrice, and geLimit.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,26 +1,49 @@
 ﻿using Figgle;
 
-string item;
-int buyPrice;
-int sellPrice;
-int geLimit;
-
-string askString(string text)
+static void flipCalc()
 {
-    Console.WriteLine(text);
-    return (Console.ReadLine());
+    int a;
+    string item;
+    string buyPrice;
+    string sellPrice;
+    string geLimit;
+    bool result;
+
+    string askString(string text)
+    {
+        Console.WriteLine(text);
+        return (Console.ReadLine());
+    }
+
+    do
+    {
+        Console.WriteLine(
+    FiggleFonts.Standard.Render("RuneScape Flip Calc"));
+
+        item = askString("Please provide the item you wish to flip:");
+
+        do
+        {
+            buyPrice = askString("Buy price:");
+            result = int.TryParse(buyPrice, out a);
+        } while (!result);
+
+        do
+        {
+            sellPrice = askString("Sell price:");
+            result = int.TryParse(sellPrice, out a);
+        } while (!result);
+
+        do
+        {
+            geLimit = askString("GE Limit:");
+            result = int.TryParse(geLimit, out a);
+        } while (!result);
+
+        Console.WriteLine($"If you flip {geLimit} {item} for the provided prices you will make {(Convert.ToInt32(sellPrice) - Convert.ToInt32(buyPrice)) * Convert.ToInt32(geLimit)})");
+    } while (true);
 }
 
 
-Console.WriteLine(
-    FiggleFonts.Standard.Render("RuneScape Flip Calc"));
-
-item = askString("Please provide the item you wish to flip:");
-buyPrice = Convert.ToInt32(askString("Buy price:"));
-sellPrice = Convert.ToInt32(askString("Sell price:"));
-geLimit = Convert.ToInt32(askString("GE Limit:"));
-
-Console.WriteLine($"If you flip {geLimit} {item} for the provided prices you will make {(sellPrice - buyPrice) * geLimit}");
-
-
+flipCalc();
 
